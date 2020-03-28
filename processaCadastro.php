a2af8bda490fad695d6a3d10fa4496edbd90e8ae
Sonia Mara de Sa <soniamarasa@hotmail.com>
2020-03-27 21:17:22 -0300

    Commit 11

---
+++ processaCadastro.php
@@ -0,0 +1,59 @@
+<?php
+
+$img= $_REQUEST['img'];
+$nome= $_REQUEST['nome'];
+$cpf= $_REQUEST['cpf'];
+$dataNasc= $_REQUEST['dataNasc'];
+$genero= $_REQUEST['genero'];
+$tel= $_REQUEST['phonenumber'];
+$cel= $_REQUEST['celnumber'];
+$cep= $_REQUEST['cep'];
+$logradouro= $_REQUEST['logradouro'];
+$numero= $_REQUEST['numerores'];
+$bairro= $_REQUEST['bairro'];
+$complemento= $_REQUEST['complemento'];
+$cidade= $_REQUEST['cidade'];
+$estado= $_REQUEST['uf'];
+$pais= $_REQUEST['pais'];
+$email= $_REQUEST['email'];
+$senha= $_REQUEST['password'];
+
+
+
+echo $img;
+echo "<br/>";
+echo $nome;
+echo "<br/>";
+echo $cpf;
+echo "<br/>";
+echo $dataNasc;
+echo "<br/>";
+echo $genero;
+echo "<br/>";
+echo $tel;
+echo "<br/>";
+echo $cel;
+echo "<br/>";
+echo $cep;
+echo "<br/>";
+echo $logradouro;
+echo "<br/>";
+echo $numero;
+echo "<br/>";
+echo $bairro;
+echo "<br/>";
+echo $complemento;
+echo "<br/>";
+echo $cidade;
+echo "<br/>";
+echo $estado;
+echo "<br/>";
+echo $pais;
+echo "<br/>";
+echo $email;
+echo "<br/>";
+echo $senha;
+
+
+
+?>
